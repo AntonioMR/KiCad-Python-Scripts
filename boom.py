678abc68a47e7f576a55bdfd6b97b349b2906008
crisconru <crisconru@gmail.com>
2017-07-30 09:24:10 +0200

    Modificada la función crear, para que sea Pythonic

--- boom.py
+++ boom.py
@@ -6,29 +6,24 @@ class Boom:
     def crear(self, fichero):
     	""" Crea un diccionario con los valores y referencias del ficher netlist especificado como parametro """
         try:
-            f = open(fichero , "r")
-        except:
+            with open(file=fichero, mode='r') as f:
+                linea_anterior = False
+                for linea in f:
+                    if '(comp (ref' in linea:
+                        finNombre = linea[15:].find(")")
+                        ref = linea[15:(15+finNombre)]
+                        linea_anterior = True
+                    elif linea_anterior:
+                        finNombre = linea[13:].find(")")
+                        valor = linea[13:(13+finNombre)]
+                        self.boom.setdefault(valor,[])
+                        self.boom[valor].append(ref)
+                        linea_anterior = False
+        except IOError as e:
+            print('Error abrindo {} = {}'.format(fichero, e))
             return False
-
-        while True:
-            linea = f.readline()
-            if not linea: 
-	            break
-	
-            elif "(comp (ref" in linea:
-                finNombre = linea[15:].find(")")
-                ref = linea[15:(15+finNombre)]
-        
-                linea = f.readline()
-                finNombre = linea[13:].find(")")
-                valor = linea[13:(13+finNombre)]
-             
-                self.boom.setdefault(valor,[])
-                self.boom[valor].append(ref)
-
-        f.close()
-
-        return True
+        else:
+            return True
 
 
     def imprimir(self):
